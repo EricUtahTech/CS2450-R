148fc42a08514753b49a6d9ad8ec911bb6209c7c
rickeay <coofaroo2@@gmail.com>
2026-01-20 12:25:45 -0700

    made the program narrow the guess range with each guess

--- age.py
+++ age.py
@@ -1,12 +1,18 @@
 import random
 x=input("What is your name?: ")
-
+low=15
+high=31
 while True:
-    y=random.randrange(15,31)
-    if input("Is your age "+str(y)+"? y/n ")=="y":
+    y=random.randrange(low,high)
+    z=input("Is your age "+str(y)+"? y for yes, h for too high, l for too low: ")
+    if z=="y":
         print(x+" is "+str(y)+" years old.")
         break
-    else:
+    elif z=="h":
+        high=y
+        print("Rats, let's try again...")
+    elif z=="l":
+        low=y+1
         print("Rats, let's try again...")
     
     
